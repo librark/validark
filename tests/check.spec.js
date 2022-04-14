d7577acfe0aa95945a4c728231d8ff429874f158
Esteban Echeverry <eecheverry@knowark.com>
2022-04-14 22:06:32 +0100

    Move check to its own function.

---
+++ tests/check.spec.js
@@ -0,0 +1,22 @@
+import { check } from '../lib/check.js'
+
+describe('check', () => {
+  it('checks the truthyness of a value', () => {
+    const value = 'Truthy Value'
+
+    const result = check(value)
+
+    expect(result).toBe(value)
+  })
+
+  it('throws an error if the value provided is not truthy', () => {
+    const value = undefined
+    const message = 'Invalid value!'
+
+    expect(() => check(value, message)).toThrow(message)
+  })
+
+  it('throws if called without arguments', () => {
+    expect(() => check()).toThrow('')
+  })
+})
